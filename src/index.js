b4470d55c00a0235604157a21aa5b86b0b348300
Tramnvph14967 <94088002+Tramnvph14967@users.noreply.github.com>
2022-04-07 16:21:10 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-
-import './assets/img/favicon.png';
-import './assets/img/apple-touch-icon.png';
-
-import './assets/vendor/aos/aos.css';
-import './assets/vendor/bootstrap/css/bootstrap.min.css';
-import './assets/vendor/bootstrap-icons/bootstrap-icons.css';
-import './assets/vendor/boxicons/css/boxicons.min.css';
-import './assets/vendor/glightbox/css/glightbox.min.css';
-import './assets/vendor/swiper/swiper-bundle.min.css';
-import './assets/css/style.css';
+import './assets/img/favicon.png'
+import './assets/img/apple-touch-icon.png'
+import './assets/vendor/aos/aos.css'
+import './assets/vendor/bootstrap/css/bootstrap.min.css'
+import './assets/vendor/bootstrap-icons/bootstrap-icons.css'
+import './assets/vendor/boxicons/css/boxicons.min.css'
+import './assets/vendor/glightbox/css/glightbox.min.css'
+import './assets/vendor/swiper/swiper-bundle.min.css'
+import './assets/css/style.css'
+ 
 
 
 
@@ -32,24 +30,4 @@ ReactDOM.render(
 
 
 
-
-
-
-// import './assets/vendor/purecounter/purecounter.js';
-// import './assets/vendor/aos/aos.js';
-// import './assets/vendor/bootstrap/js/bootstrap.bundle.min.js';
-// import './assets/vendor/glightbox/js/glightbox.min.js';
-// import './assets/vendor/isotope-layout/isotope.pkgd.min.js';
-// import './assets/vendor/swiper/swiper-bundle.min.js';
-// import './assets/vendor/typed.js/typed.min.js';
-// import './assets/vendor/waypoints/noframework.waypoints.js';
-// import './assets/vendor/php-email-form/validate.js';
-// // import './assets/js/main.js';
-
-
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
